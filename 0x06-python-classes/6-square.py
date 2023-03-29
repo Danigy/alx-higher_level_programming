5ffccfcd1bd448c0552d45ee1da7824efe9dd29c
Danigy <dnlmdwrk@gmail.com>
2023-03-29 13:55:17 -0700

    6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,57 +1,91 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# (C) 2023  Alx-Software Engineering Class, batch 11
+#           Repository: alx-higher_level_programming
+#           Directory: 0x06-python-classes
+# -----------------------------------------------------------
+"""Square Class.
+This module contains a class that defines a square.
+Usage Example:
+    Square = __import__('6-square').Square
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+"""
 
-"""Define a class Square"""
 
 class Square:
-    """Represent a square"""
+    """Defines the blueprint of a square.
+    Attribute:
+        size (int): An integer representing the object size.
+        position (int, int): The position of the new square.
+    """
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square
-        Args:
-            size (int): The size of the new square.
-            position (tuple): The position of the new square
-        """
-        self.size = size
-        self.position = position
+        """An object constructor method."""
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
-        """Get/set the current size of the square"""
-        return self.__size
+        """Gets the size private attribute value.
+        Returns:
+            The size private attribute
+        """
+        return (self.__size)
 
     @size.setter
     def size(self, value):
+        """Sets the size private attribute value.
+        Validates the assignment of the size private attribute.
+        Arg:
+            value: the value to be set
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     @property
     def position(self):
-        """Get/set the current position of the square"""
+        """Gets the position private attribute value.
+        Returns:
+            The position private attribute
+        """
         return (self.__position)
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-                len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+        """Sets the position private attribute value.
+        Validates the assignment of the position private attribute.
+        Arg:
+            value: the value to be set
+        """
+        if (
+            not isinstance(value, tuple)
+            or len(value) != 2
+            or not all(isinstance(num, int) for num in value)
+            or not all(num >= 0 for num in value)
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Return the current area of the square"""
-        return (self.__size ** 2)
+        """A public object method.
+        Returns:
+            The current square area
+        """
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square with the # character"""
+        """Displays the square object with # character"""
         if self.__size == 0:
-            print()
+            print("")
             return
 
-        print("\n" * self.__position[1], end="")
-        for i in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
