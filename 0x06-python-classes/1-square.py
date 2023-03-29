d086b16fb5b83e8abf1991370d729ed83879f1a9
Danigy <dnlmdwrk@gmail.com>
2023-03-29 13:07:18 -0700

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-
-"""Define a class Square"""
+# -----------------------------------------------------------
+# (C) 2023  Alx-Software Engineering Class, batch 11
+#           Repository: alx-higher_level_programming
+#           Directory: 0x06-python-classes
+# -----------------------------------------------------------
+"""Square Class.
+This module contains a class that defines a square.
+Usage Example:
+    Square = __import__('1-square').Square
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+"""
 
 class Square:
-    """Represent a square"""
+    """Defines the blueprint of a square.
+    Attribute:
+        size: An integer indicating the size of the square object.
+    """
 
     def __init__(self, size):
-        """Initialize a new Square
-        Args:
-            size (int): The size of the new square
+        """An object constructor method.
+        Initiatilizes Square with size.
+        Arg:
+            size: A integer representing object size
         """
         self.__size = size
